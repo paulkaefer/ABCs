82fb320679c92e4d85305e225652031ac7285955
Paul Kaefer <paul.kaefer@gmail.com>
2015-05-27 14:28:20 -0500

    added initial letters options (lower, upper)

--- abcs.c
+++ abcs.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
 
@@ -7,13 +8,17 @@ int main(int argc, char* argv[]) {
     int useLowercase = 1;
     int useLetterFile = 0;
     char* filename = NULL;
+    char* letters = "abcdefghijklmnopqrstuvwxyz";
 
     // parse parser directives
     int iFlag;
     for(iFlag = 0; iFlag < argc; iFlag++) {
         // commands: -upper (-lower is default); -letterFile FILE -inputFile FILE <code file>
+        // better: just pass string OR file
         if (!strcmp(argv[iFlag], "-upper")) {
             printf("using upper case letters\n");
+            letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            printf("%d\n",(int)strlen(letters));
         } else {
             if (argc == iFlag + 1) {
                 // end of arguments
